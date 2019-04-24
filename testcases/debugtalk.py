704d3e7b1cec113a357ad7094771982ea885976c
jymmmm <577177756@qq.com>
2019-04-24 13:55:12 +0800

    Signed-off-by: jymmmm <577177756@qq.com>

--- testcases/debugtalk.py
+++
@@ -1,7 +0,0 @@
-def setup_hook_request(request):
-    exposed_ids= request.get("params", {}).get("exposed_ids")
-    print(exposed_ids)
-
-def teardown_hook_response(response):
-    print(response.status_code)
-    print(response.text)
